82b86750bc7dfeb2e7c02aaea3ea372e173ac35b
Your Name <your.email@example.com>
2026-08-11 00:25:19 +0800

    Convert content.config.ts to .js

---
+++ content.config.js
@@ -0,0 +1,37 @@
+import { defineContentConfig, defineCollection } from '@nuxt/content'
+
+export default defineContentConfig({
+  collections: {
+    content: defineCollection({
+      type: 'page',
+      source: '*',
+    }),
+    articles: defineCollection({
+      type: 'page',
+      source: 'articles/**',
+    }),
+    perspectives: defineCollection({
+      type: 'page',
+      source: 'perspectives/**',
+    }),
+    pages: defineCollection({
+      type: 'page',
+      source: 'pages/**',
+    }),
+    books: defineCollection({
+      type: 'page',
+      source: 'books/**',
+    }),
+    people: defineCollection({
+      type: 'page',
+      source: 'people/**',
+    }),
+    languages: defineCollection({
+      type: 'page',
+      source: 'languages/**',
+    }),
+    mnemonics: defineCollection({
+      type: 'page',
+      source: 'mnemonics/**',
+  },
+})
